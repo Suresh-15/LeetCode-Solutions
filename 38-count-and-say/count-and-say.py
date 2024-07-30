8f5e6a632211216432ed5ad8cc17f01091caba90
Suresh_J <142098721+Suresh-15@users.noreply.github.com>
2024-07-31 01:14:50 +0530

    Time: 44 ms (52.33%) | Memory: 16.4 MB (88.31%) - LeetSync

---
+++ 38-count-and-say/count-and-say.py
@@ -0,0 +1,26 @@
+class Solution:
+    def compress(self, s: str) -> str:
+        if s == '':
+            return ''
+        count, prev, result = 0, s[0], ''
+        for i in range(len(s)):
+            if prev == s[i]:
+                count += 1
+            else:
+                result += str(count)
+                result += s[i-1]
+                prev = s[i]
+                count = 1
+        else:
+            if count != 0:
+                result += str(count)
+                result += s[i]
+        
+        print(result)
+        return result
+
+    def countAndSay(self, n: int) -> str:
+        if n == 1:
+            return "1"
+            print(self.compress('21'))
+        return self.compress(self.countAndSay(n-1))
