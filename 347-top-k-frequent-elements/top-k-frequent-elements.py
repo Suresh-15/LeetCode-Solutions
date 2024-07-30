f01c2f535d6fb7bda0a3399631fb99a059c4a8a6
Suresh_J <142098721+Suresh-15@users.noreply.github.com>
2024-07-31 01:59:32 +0530

    Time: 91 ms (46.29%) | Memory: 21.1 MB (72.16%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -0,0 +1,41 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # nums.sort(reverse = True, key= lambda x: nums.count(x))
+        """
+        dictionary = { num: nums.count(num) for num in nums }
+        output = []
+        for i in dictionary:
+            if dictionary[i] >= k:
+                output.append(i)
+        output.sort(reverse = True, key = lambda x: nums.count(x))
+        if output == []:
+            return nums
+        return output
+
+
+        freq_map = {}
+        buckets = [[] for _ in range(len(nums) + 1)]
+        top_k = []
+        for num in nums:
+            freq_map[num] = freq_map.get(num, 0) + 1
+        for num, freq in freq_map.items():
+            buckets[freq].append(num)
+        for i in range(len(buckets) - 1, 0, -1):
+            for num in buckets[i]:
+                top_k.append(num)
+                if len(top_k) == k:
+                    return top_k
+        """
+
+        from collections import defaultdict
+        d = defaultdict(int)
+        for num in nums:
+            d[num] += 1
+
+        heap = []
+        for (key, v) in d.items():
+            heappush(heap, (v,key))
+            if len(heap) > k:
+                heappop(heap)
+
+        return [k for (v, k) in heap]
