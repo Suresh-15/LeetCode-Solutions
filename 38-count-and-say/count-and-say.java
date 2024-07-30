5c567992b5ae5c9c87848453012f011ff843cbae
Suresh_J <142098721+Suresh-15@users.noreply.github.com>
2024-07-31 01:35:35 +0530

    Time: 2 ms (86.20%) | Memory: 41.4 MB (49.99%) - LeetSync

--- 38-count-and-say/count-and-say.java
+++ 38-count-and-say/count-and-say.java
@@ -2,22 +2,24 @@ class Solution {
     public String compress(String s) {
         int count = 0;
         char prev = s.charAt(0);
-        String result = "";
+        StringBuilder result = new StringBuilder("");
 
         for (int i = 0; i < s.length(); i++) {
             if (prev == s.charAt(i)) {
                 count = count + 1;
             } else {
-                result = result + String.valueOf(count) + s.charAt(i - 1);
+                result.append(count);
+                result.append(s.charAt(i - 1));
                 prev = s.charAt(i);
                 count = 1;
             }
         }
         if (count != 0) {
-            result = result + String.valueOf(count) + s.charAt(s.length() - 1);
+            result.append(count);
+            result.append(s.charAt(s.length() - 1));
         }
 
-        return result;
+        return result.toString();
     }
 
     public String countAndSay(int n) {
