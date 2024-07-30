2c743b005bb39cf80749f204ad4c19804cc1492c
Suresh_J <142098721+Suresh-15@users.noreply.github.com>
2024-07-31 02:04:30 +0530

    Time: 42 ms (88.12%) | Memory: 16.7 MB (63.40%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
@@ -1,5 +1,6 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        """
         nums1.sort()
         nums2.sort()
         
@@ -14,6 +15,18 @@ class Solution:
             else:
                 result.append(nums1[i])  
                 i += 1  
-                j += 1  
-                
+                j += 1
+        return result
+        """
+
+        freq = {}
+        for num in nums1:
+            freq[num] = freq.get(num, 0) + 1
+        
+        result = []
+        for num in nums2:
+            if freq.get(num):
+                result.append(num)
+                freq[num] -= 1
+
         return result
