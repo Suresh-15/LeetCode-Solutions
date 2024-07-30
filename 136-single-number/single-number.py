174e748a3d126438b73eaf3252e6b0733c815df4
Suresh_J <142098721+Suresh-15@users.noreply.github.com>
2024-07-30 23:37:09 +0530

    Time: 113 ms (43.19%) | Memory: 19.1 MB (48.74%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,14 +1,22 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        
+        """
         ans=0
         for i in nums:
             ans=ans^i
         return ans
-
+        """
         """
         s = sorted(list(set(nums)))
         for i in s:
             if nums.count(i) == 1:
                 return i
         """
+        nums = sorted(nums)
+        for i in range(0, len(nums), 2):
+            if i == len(nums) - 1:
+                return nums[i] 
+            if nums[i] != nums[i+1]:
+                return nums[i]
+
+
