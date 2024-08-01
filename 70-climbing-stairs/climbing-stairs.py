9c5ccb0e31df5b81b56db5d2b502377e74ed8e6b
Suresh_J <142098721+Suresh-15@users.noreply.github.com>
2024-08-02 01:30:01 +0530

    Time: 30 ms (83.74%) | Memory: 16.5 MB (52.99%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -3,7 +3,7 @@ class Solution:
         if n == 0 or n == 1:
             return 1
         prev, curr = 1, 1
-        for i in range(2, n+1):
+        for i in range(2, n + 1):
             temp = curr
             curr = prev + curr
             prev = temp
