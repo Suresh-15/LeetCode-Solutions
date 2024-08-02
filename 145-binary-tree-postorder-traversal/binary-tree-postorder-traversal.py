4fabf15847558ba65e53356e0aa779d568a51760
Suresh_J <142098721+Suresh-15@users.noreply.github.com>
2024-08-02 16:42:35 +0530

    Time: 33 ms (71.92%) | Memory: 16.5 MB (53.24%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.py
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.py
@@ -6,6 +6,19 @@
 #         self.right = right
 class Solution:
     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        
+        """
         if root is None:
             return []
         return self.postorderTraversal(root.left) + self.postorderTraversal(root.right) + [root.val]
+        """
+
+        result = []
+        def postorder(root):
+            if not root:
+                return None
+            postorder(root.left)
+            postorder(root.right)
+            result.append(root.val)
+        postorder(root)
+        return result
