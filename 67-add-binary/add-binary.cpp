088081694293748fe20c63980cc78e8746f58ba4
Suresh_J <142098721+Suresh-15@users.noreply.github.com>
2024-08-04 00:16:39 +0530

    Time: 5 ms (27.30%) | Memory: 10.3 MB (26.77%) - LeetSync

--- 67-add-binary/add-binary.cpp
+++ 67-add-binary/add-binary.cpp
@@ -1,34 +1,24 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        int gap = a.length() - b.length();
-        if (gap < 0) {
-            string c = b;
-            b = a;
-            a = c;
-            gap = -1 * gap;
-        }
+        string result;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
 
-        for (int i = 0; i < gap; i++) {
-            b = '0' + b;
-        }
+        while (i >= 0 || j >= 0) {
+            int sum = carry;
+            if (i >= 0)
+                sum += a[i--] - '0';
+            if (j >= 0)
+                sum += b[j--] - '0';
 
-        bool carry = false;
-        string result = "";
-        for (int i = a.length() - 1; i >= 0; i--) {
-            if (a[i] == '1' && b[i] == '1') {
-                result = (carry ? '1' : '0') + result;
-                carry = true;
-            } else if (a[i] == '0' && b[i] == '0') {
-                result = (carry ? '1' : '0') + result;
-                carry = false;
-            } else {
-                result = (carry ? '0' : '1') + result;
-            }
+            carry = sum > 1 ? 1 : 0;
+            result += to_string(sum % 2);
         }
-
         if (carry)
-            result = '1' + result;
+            result += to_string(carry);
+        reverse(result.begin(), result.end());
         return result;
     }
 };
