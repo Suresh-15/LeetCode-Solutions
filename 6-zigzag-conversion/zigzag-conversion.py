0dc57c4f37ac03ebee9c87e129550197c423054c
Suresh J <142098721+Suresh-15@users.noreply.github.com>
2024-08-05 20:49:18 +0530

    Time: 52 ms (54.97%) | Memory: 16.5 MB (97.60%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.py
+++ 6-zigzag-conversion/zigzag-conversion.py
@@ -1,5 +1,7 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
+        """
+
         m = [[] for _ in range(numRows)]
         index, zigzag = 0, 0
         if numRows == 1 or numRows >= len(s):
@@ -17,3 +19,22 @@ class Solution:
             m[i] = "".join(m[i])
 
         return "".join(m)
+
+        """
+
+        if numRows < 2:
+            return s
+        arr = ["" for i in range(numRows)]
+        direction = "down"
+        row = 0
+        for i in s:
+            arr[row] += i
+            if row == numRows - 1:
+                direction = "up"
+            elif row == 0:
+                direction = "down"
+            if direction == "down":
+                row += 1
+            else:
+                row -= 1
+        return "".join(arr)
