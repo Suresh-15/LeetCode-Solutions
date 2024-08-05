bcc98a5f30185c401ff44d7e1b6550033e3a7a06
Suresh J <142098721+Suresh-15@users.noreply.github.com>
2024-08-05 21:09:26 +0530

    Time: 1 ms (100.00%) | Memory: 44.3 MB (96.31%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.java
+++ 6-zigzag-conversion/zigzag-conversion.java
@@ -1,38 +1,38 @@
 class Solution {
     public String convert(String s, int numRows) {
         /**
-        List<Character>[] al = new ArrayList[numRows];
-        if (numRows == 1 || numRows >= s.length())
-            return s;
-
-        for(int i=0; i < numRows; i++)
-            al[i] = new ArrayList<>();
-
-        int index = 0, zigzag = 0;
-        for (char x: s.toCharArray()){
-            al[index].add(x);
-            if (index == numRows - 1)
-                zigzag = -1;
-            else if (index == 0)
-                zigzag = 1;
-            index += zigzag;
-        }
-
-        StringBuilder result = new StringBuilder();
-        for (List<Character> m: al){
-            for (char c: m){
-                result.append(c);
-            }
-        }
-
-        return result.toString();
-
-        
+         * List<Character>[] al = new ArrayList[numRows];
+         * if (numRows == 1 || numRows >= s.length())
+         * return s;
+         * 
+         * for(int i=0; i < numRows; i++)
+         * al[i] = new ArrayList<>();
+         * 
+         * int index = 0, zigzag = 0;
+         * for (char x: s.toCharArray()){
+         * al[index].add(x);
+         * if (index == numRows - 1)
+         * zigzag = -1;
+         * else if (index == 0)
+         * zigzag = 1;
+         * index += zigzag;
+         * }
+         * 
+         * StringBuilder result = new StringBuilder();
+         * for (List<Character> m: al){
+         * for (char c: m){
+         * result.append(c);
+         * }
+         * }
+         * 
+         * return result.toString();
+         * 
          */
-        
+
         if (numRows == 1 || s.length() < numRows) {
             return s;
         }
+
         int len = s.length();
         char[] result = new char[len];
         int diff = (numRows - 1) * 2;
@@ -46,10 +46,10 @@ class Solution {
                 if (i == 0 || i == (numRows - 1)) {
                     temp += diff;
                 } else {
-                    temp += diff - i*2;
+                    temp += diff - i * 2;
                     if (temp < len) {
                         result[count++] = s.charAt(temp);
-                        temp += i*2;
+                        temp += i * 2;
                     }
                 }
             }
