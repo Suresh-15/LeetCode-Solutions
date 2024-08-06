c78323c1d7556f48c6635b9a33b962629c6bef0a
Suresh J <142098721+Suresh-15@users.noreply.github.com>
2024-08-07 04:02:32 +0530

    Time: 95 ms (39.93%) | Memory: 16.6 MB (43.23%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,105 +1,19 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        for i in range(9):
-            if len(set(board[i])) != 9 - board[i].count(".") + 1:
-                return False
+        cube = defaultdict(set)
+        rows = defaultdict(set)
+        cols = defaultdict(set)
 
         for i in range(9):
-            l = [board[x][i] for x in range(9)]
-            if len(set(l)) != 9 - l.count(".") + 1:
-                return False
-
-
-
-        seen, count = set(), 0
-        for i in range(0, 3):
-            for j in range(0, 3):
-                if board[i][j] != ".":
-                    seen.add(board[i][j])
-                    count += 1
-        
-        if len(seen) != count:
-            return False
-
-        seen, count = set(), 0
-        for i in range(0, 3):
-            for j in range(3, 6):
-                if board[i][j] != ".":
-                    seen.add(board[i][j])
-                    count += 1
-        
-        if len(seen) != count:
-            return False
-
-        seen, count = set(), 0
-        for i in range(0, 3):
-            for j in range(6, 9):
-                if board[i][j] != ".":
-                    seen.add(board[i][j])
-                    count += 1
-        
-        if len(seen) != count:
-            return False
-
-        seen, count = set(), 0
-        for i in range(3, 6):
-            for j in range(0, 3):
-                if board[i][j] != ".":
-                    seen.add(board[i][j])
-                    count += 1
-        
-        if len(seen) != count:
-            return False
-
-        seen, count = set(), 0
-        for i in range(3, 6):
-            for j in range(3, 6):
-                if board[i][j] != ".":
-                    seen.add(board[i][j])
-                    count += 1
-        
-        if len(seen) != count:
-            return False
-
-        seen, count = set(), 0
-        for i in range(3, 6):
-            for j in range(6, 9):
-                if board[i][j] != ".":
-                    seen.add(board[i][j])
-                    count += 1
-        
-        if len(seen) != count:
-            return False
-
-        seen, count = set(), 0
-        for i in range(6, 9):
-            for j in range(0, 3):
-                if board[i][j] != ".":
-                    seen.add(board[i][j])
-                    count += 1
-        
-        if len(seen) != count:
-            return False
-
-        seen, count = set(), 0
-        for i in range(6, 9):
-            for j in range(3, 6):
-                if board[i][j] != ".":
-                    seen.add(board[i][j])
-                    count += 1
-        
-        if len(seen) != count:
-            return False
-
-        seen, count = set(), 0
-        for i in range(6, 9):
-            for j in range(6, 9):
-                if board[i][j] != ".":
-                    seen.add(board[i][j])
-                    count += 1
-        
-        if len(seen) != count:
-            return False
-
+            for j in range(9):
+                element = board[i][j]
+                if element.isalnum():
+                    k = i // 3 * 3 + j // 3
+                    if element in rows[i] or element in cols[j] or element in cube[k]:
+                        return False
+                    else:
+                        rows[i].add(element)
+                        cols[j].add(element)
+                        cube[k].add(element)
 
         return True
