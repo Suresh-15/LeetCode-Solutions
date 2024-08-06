8f25f3182f291016b10772b5c3de06362f137441
Suresh J <142098721+Suresh-15@users.noreply.github.com>
2024-08-06 23:51:11 +0530

    Time: 39 ms (29.21%) | Memory: 16.5 MB (61.85%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.py
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.py
@@ -8,10 +8,20 @@ class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if head is None or head.next is None:
             return head
-        result = head
-        while result and result.next:
-            temp = result.val
-            result.val = result.next.val
-            result.next.val = temp
-            result = result.next.next
-        return head
+
+        result, lag = head.next, ListNode(0)
+        left, right = head, head.next
+
+        while left and left.next:
+            temp = right.next
+            right.next = left
+            left.next = temp
+            lag.next = right
+            lag = left
+            left = left.next
+            if left:
+                right = left.next
+            else:
+                break
+            
+        return result
