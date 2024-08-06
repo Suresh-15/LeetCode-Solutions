624a660933c0bb9ecef004440e6696970a235578
Suresh J <142098721+Suresh-15@users.noreply.github.com>
2024-08-07 04:05:06 +0530

    Time: 91 ms (63.93%) | Memory: 16.5 MB (80.80%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -8,12 +8,11 @@ class Solution:
             for j in range(9):
                 element = board[i][j]
                 if element.isalnum():
-                    k = i // 3 * 3 + j // 3
-                    if element in rows[i] or element in cols[j] or element in cube[k]:
+                    if element in rows[i] or element in cols[j] or element in cube[(i // 3, j // 3)]:
                         return False
                     else:
                         rows[i].add(element)
                         cols[j].add(element)
-                        cube[k].add(element)
+                        cube[(i // 3, j // 3)].add(element)
 
         return True
