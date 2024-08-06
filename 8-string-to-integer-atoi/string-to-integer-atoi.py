262cc1700c4ce229dd857f343de2d461839cafd6
Suresh J <142098721+Suresh-15@users.noreply.github.com>
2024-08-06 22:51:06 +0530

    Time: 36 ms (71.23%) | Memory: 16.5 MB (86.80%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/string-to-integer-atoi.py
@@ -1,36 +1,26 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        num = '0123456789'
-        s = s.strip()
+        # s = s.strip()
         result = ""
-        for x in s:
-            if x != ' ' and (x in '-+' or x in num) and len(result) == 0:
-                result += x
-            elif x in num:
-                result += x
-            else:
-                break
-            
-        if result == '' or result in '-+':
+        if len(s) <= 1 and not(s.isnumeric()):
             return 0
-        else:
-            sign = 1
-            if result[0] in '+-':
-                sign = -1 if result[0] == '-' else 1
-                result = result[1:]
-        
-            def str_to_int(num_str):
-                result = 0
-                for char in num_str:
-                    result = result * 10 + (ord(char) - ord('0')) 
-                return result
-
-            num_val = str_to_int(result) * sign
-            
-            INT_MIN, INT_MAX = -(2**31), (2**31 - 1)
-            if num_val < INT_MIN:
-                return INT_MIN
-            elif num_val > INT_MAX:
-                return INT_MAX
+        for i in s:
+            if i == " " and len(result) == 0:
+                continue
+            elif (i == '+' or i == '-') and len(result) == 0:
+                result += i
             else:
-                return num_val
+                if i.isnumeric():
+                    result += i
+                else:
+                    break
+
+        if len(result) == 0 or result == '+' or result == '-' or result=="":
+            return 0
+        if int(result) < pow(-2, 31):
+            return pow(-2, 31)
+        elif int(result) > pow(2, 31) - 1:
+            return pow(2, 31) - 1
+        else:
+            return int(result)
+            
