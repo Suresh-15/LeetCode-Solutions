b2e5cdc792125a304cbf3b73c731e04f7c041fb8
Suresh J <142098721+Suresh-15@users.noreply.github.com>
2024-08-08 21:25:05 +0530

    Time: 87 ms (92.85%) | Memory: 16.8 MB (35.89%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.py
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.py
@@ -3,7 +3,7 @@ class Solution:
         diagonalSum, n = 0, len(mat)
         for i in range(n):
             diagonalSum += mat[i][i]
-            diagonalSum += mat[i][n - 1 - i]
+            diagonalSum += mat[i][-1*(i+1)]
         if n % 2 == 1:
             diagonalSum -= mat[n // 2][n // 2]
         return diagonalSum
