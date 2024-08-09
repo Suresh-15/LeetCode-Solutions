c77b7822884d1314b274823a61717c308e5a68cc
Suresh J <142098721+Suresh-15@users.noreply.github.com>
2024-08-09 11:38:39 +0530

    Time: 1 ms (100.00%) | Memory: 42.7 MB (81.65%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,16 +1,48 @@
 class Solution {
+    /*
+     * public boolean isPalindrome(String s) {
+     * StringBuilder word = new StringBuilder();
+     * for (int i = 0; i < s.length(); i++) {
+     * if (Character.isLetterOrDigit(s.charAt(i))) {
+     * word.append(Character.toLowerCase(s.charAt(i)));
+     * }
+     * }
+     * int n = word.length();
+     * for (int i = 0; i < n / 2; i++) {
+     * if (word.charAt(i) != word.charAt(n - 1 - i))
+     * return false;
+     * }
+     * return true;
+     * }
+     */
+
     public boolean isPalindrome(String s) {
-        StringBuilder word = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-            if (Character.isLetterOrDigit(s.charAt(i))) {
-                word.append(Character.toLowerCase(s.charAt(i)));
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            char iChar = s.charAt(i);
+            char jChar = s.charAt(j);
+            if (!isValid(iChar)) {
+                i++;
+            } else if (!isValid(jChar)) {
+                j--;
+            } else if (lower(iChar) != lower(jChar)) {
+                return false;
+            } else {
+                i++;
+                j--;
             }
         }
-        int n = word.length();
-        for (int i = 0; i < n / 2; i++) {
-            if (word.charAt(i) != word.charAt(n - 1 - i))
-                return false;
-        }
         return true;
+    }
+
+    boolean isValid(char c) {
+        return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9');
+    }
+
+    char lower(char c) {
+        if (c >= 'A' && c <= 'Z') {
+            return (char) (c + 32);
+        }
+        return c;
     }
 }
