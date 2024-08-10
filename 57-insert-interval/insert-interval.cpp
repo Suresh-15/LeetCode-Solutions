c81f4dea24a33a8c22de63f79a0511866b7c0917
Suresh J <142098721+Suresh-15@users.noreply.github.com>
2024-08-10 08:36:02 +0530

    Time: 13 ms (47.19%) | Memory: 20.8 MB (24.36%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -4,33 +4,21 @@ public:
                                vector<int>& newInterval) {
 
         vector<vector<int>> result;
-        int newStart = newInterval[0];
-        int newEnd = newInterval[1];
-        int i = 0, n = intervals.size();
-
-        while (i < n && newStart > intervals[i][0]) {
+        int i = 0;
+        while (i < intervals.size() && intervals[i][1] < newInterval[0]) {
             result.push_back(intervals[i]);
             i++;
         }
-
-        if (result.empty() || result.back()[1] < newStart) {
-            result.push_back(newInterval);
-        } else {
-            result.back()[1] = max(result.back()[1], newEnd);
-        }
-
-        while (i < n) {
-            int start = intervals[i][0];
-            int end = intervals[i][1];
-
-            if (result.back()[1] < start) {
-                result.push_back(intervals[i]);
-            } else {
-                result.back()[1] = max(result.back()[1], end);
-            }
+        while (i < intervals.size() && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = std::min(newInterval[0], intervals[i][0]);
+            newInterval[1] = std::max(newInterval[1], intervals[i][1]);
             i++;
         }
-
+        result.push_back(newInterval);
+        while (i < intervals.size()) {
+            result.push_back(intervals[i]);
+            i++;
+        }
         return result;
     }
 };
