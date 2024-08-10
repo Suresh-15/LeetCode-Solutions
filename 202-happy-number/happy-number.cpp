9dbd1cdb5f7c53478194aceebe7bce604ecc76f8
Suresh J <142098721+Suresh-15@users.noreply.github.com>
2024-08-10 08:03:29 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (79.31%) - LeetSync

---
+++ 202-happy-number/happy-number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        int slow = n, fast = n;
+        do {
+            slow = sum_of_squares(slow);
+            fast = sum_of_squares(sum_of_squares(fast));
+            if (slow == 1) {
+                return true;
+            }
+        } while (slow != fast);
+        return n == 1;
+    }
+
+    int sum_of_squares(int i) {
+        int m, sqs = 0;
+        while (i > 0) {
+            m = i % 10;
+            sqs += m * m;
+            i = i / 10;
+        }
+        return sqs;
+    }
+};
