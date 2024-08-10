e4c170d683480a71fb4036a9a9332426f09b8a44
Suresh J <142098721+Suresh-15@users.noreply.github.com>
2024-08-10 10:22:11 +0530

    Time: 39 ms (28.86%) | Memory: 16.6 MB (18.64%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.py
+++ 119-pascals-triangle-ii/pascals-triangle-ii.py
@@ -1,5 +1,15 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
+        res = [[1]]
+        for i in range (rowIndex):
+            template = [0] + res[-1] + [0]
+            row = []
+            for j in range (len(res[-1]) + 1):
+                row.append(template[j] + template[j+1])
+            res.append(row)
+        return res[-1]
+
+        """
         def factorial(i):
             fact = 1
             for i in range(1, i+1):
@@ -13,3 +23,4 @@ class Solution:
             result.append(numerator // denominator)
         result.append(1)
         return result
+        """
