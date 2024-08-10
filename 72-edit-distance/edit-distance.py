c34a70b6b08804235aa0bfbd70d877a8a3dff51d
Suresh J <142098721+Suresh-15@users.noreply.github.com>
2024-08-10 22:16:34 +0530

    Time: 105 ms (48.52%) | Memory: 20.3 MB (13.18%) - LeetSync

--- 72-edit-distance/edit-distance.py
+++ 72-edit-distance/edit-distance.py
@@ -1,6 +1,5 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        """
         cache = [[float("inf")] * (len(word2) + 1) for _ in range(len(word1) + 1)]
         for col in range(len(word2) + 1):
             cache[len(word1)][col] = len(word2) - col
@@ -16,18 +15,3 @@ class Solution:
                     )
 
         return cache[0][0]
-        """
-
-        @lru_cache(None)
-        def helper(p1, p2):
-            if p1 == -1:
-                return p2 + 1
-            if p2 == -1:
-                return p1 + 1
-            if word1[p1] == word2[p2]:
-                return helper(p1 - 1, p2 - 1)
-            return (
-                min(helper(p1 - 1, p2), helper(p1, p2 - 1), helper(p1 - 1, p2 - 1)) + 1
-            )
-
-        return helper(len(word1) - 1, len(word2) - 1)
