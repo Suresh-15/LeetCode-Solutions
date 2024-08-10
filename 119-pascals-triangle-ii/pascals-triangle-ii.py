f5b88b02bf5ec0fb6ba45d241cb27581ecba176e
Suresh J <142098721+Suresh-15@users.noreply.github.com>
2024-08-10 10:23:26 +0530

    Time: 40 ms (22.09%) | Memory: 16.5 MB (18.64%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.py
+++ 119-pascals-triangle-ii/pascals-triangle-ii.py
@@ -13,7 +13,7 @@ class Solution:
 
         def factorial(i):
             fact = 1
-            for i in range(1, i+1):
+            for i in range(1, i + 1):
                 fact *= i
             return fact
 
