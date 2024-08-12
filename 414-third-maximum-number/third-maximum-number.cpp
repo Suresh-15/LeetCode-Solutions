ceff97a2c7b2171154675a3a2aee6ed92014b27e
Suresh J <142098721+Suresh-15@users.noreply.github.com>
2024-08-13 00:19:26 +0530

    Time: 8 ms (32.88%) | Memory: 11.8 MB (53.37%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.cpp
+++ 414-third-maximum-number/third-maximum-number.cpp
@@ -1,36 +1,14 @@
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
-        set<int> s;
-        bool f = 0;
-        for (int i : nums) {
-            if (i == -2147483648)
-                f = 1;
-            s.insert(i);
-        }
+        sort(nums.begin(), nums.end());
+        nums.erase(unique(nums.begin(), nums.end()), nums.end());
+        int n = nums.size();
 
-        if (s.size() == 2) {
-            int a = INT_MIN;
-            for (int i : s)
-                a = i;
-            return a;
+        if (n < 3) {
+            return nums[n - 1];
+        } else {
+            return nums[n - 3];
         }
-
-        int max1 = INT_MIN, max2 = INT_MIN, max3 = INT_MIN;
-        for (int i : s) {
-            if (i > max1) {
-                max3 = max2;
-                max2 = max1;
-                max1 = i;
-            } else if (i < max1 and i > max2) {
-                max3 = max2;
-                max2 = i;
-            } else if (i < max2 and i > max3)
-                max3 = i;
-        }
-
-        if (max3 == -2147483648 and f == 0)
-            return max1;
-        return max3;
     }
 };
