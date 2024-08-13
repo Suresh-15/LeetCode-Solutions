f59be9cc5362149fbe9b2ebbeb347c6a39642945
Suresh J <142098721+Suresh-15@users.noreply.github.com>
2024-08-13 09:00:33 +0530

    Time: 43 ms (48.45%) | Memory: 16.6 MB (70.01%) - LeetSync

--- 415-add-strings/add-strings.py
+++ 415-add-strings/add-strings.py
@@ -1,8 +1,12 @@
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
+        sys.set_int_max_str_digits(10000)
+        return str(int(num1) + int(num2))
+
+        """
         result = ""
         l1, l2, carry = len(num1) - 1, len(num2) - 1, 0
-        
+
         while l1 >= 0 or l2 >= 0 or carry > 0:
             n1 = int(num1[l1]) if l1 >= 0 else 0
             n2 = int(num2[l2]) if l2 >= 0 else 0
@@ -13,3 +17,4 @@ class Solution:
             l2 -= 1
 
         return result
+        """
