38e2aec3468c49070ac4ce30b888ca8004ab60a3
Suresh J <142098721+Suresh-15@users.noreply.github.com>
2024-08-13 23:09:29 +0530

    Time: 40 ms (70.34%) | Memory: 16.7 MB (35.24%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -1,15 +1,12 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-
-        replace = {}
+        dictionary = {}
         for i in range(len(s)):
-            if s[i] not in replace.keys() and t[i] in replace.values():
+            if s[i] not in dictionary:
+                if t[i] not in dictionary.values():
+                    dictionary[s[i]] = t[i]
+                else:
+                    return False
+            elif dictionary[s[i]] != t[i]:
                 return False
-            elif s[i] not in replace.keys():
-                replace[s[i]] = t[i]
-            elif replace[s[i]] != t[i]:
-                return False
-        else:
-            return True
+        return True
