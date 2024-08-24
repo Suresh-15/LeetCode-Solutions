39168285cbbd32cd74e519104142ab1daf3439d0
Suresh J <142098721+Suresh-15@users.noreply.github.com>
2024-08-24 18:46:19 +0530

    Time: 63 ms (5.44%) | Memory: 16.6 MB (44.53%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -2,7 +2,6 @@ class Solution:
     def romanToInt(self, s: str) -> int:
         V = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
         result, i, n = 0, 0, len(s)
-
         while i < n:
             if i < n - 1 and V[s[i]] < V[s[i + 1]]:
                 result += V[s[i + 1]] - V[s[i]]
@@ -10,5 +9,5 @@ class Solution:
             else:
                 result += V[s[i]]
                 i += 1
-        else:
-            return result
+        
+        return result
