58b05f92f87088d3fb5bf845e9cfe48ac2a2a56a
Suresh J <142098721+Suresh-15@users.noreply.github.com>
2024-08-25 19:36:20 +0530

    Time: 3 ms (44.76%) | Memory: 8 MB (31.09%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        string result = "";
-        int minIndex = min(word1.length(), word2.length());
-        for (int i = 0; i < minIndex; i++) {
-            result += word1[i];
-            result += word2[i];
+        string answer = "";
+        int total = word1.size() + word2.size();
+        int i = 0, index1 = word1.size(), index2 = word2.size();
+        int n1 = word1.size(), n2 = word2.size();
+
+        while (answer.size() != total) { 
+            if (n2 > n1 && i > n1 - 1) {
+                answer.push_back(word2[index1]);
+                index1++;
+            } else if (n1 > n2 && i > n2 - 1) {
+                answer.push_back(word1[index2]);
+                index2++;
+            } else {
+                answer.push_back(word1[i]);
+                answer.push_back(word2[i]);
+                i++;
+            }
         }
-        if (word1.length() < word2.length()) {
-            for (int i = minIndex; i < word2.length(); i++)
-                result += word2[i];
-        } else if (word2.length() < word1.length()) {
-            for (int i = minIndex; i < word1.length(); i++)
-                result += word1[i];
-        }
-        return result;
+        return answer;
     }
 };
