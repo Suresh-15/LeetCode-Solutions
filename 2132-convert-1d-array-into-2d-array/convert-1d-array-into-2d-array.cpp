16cc7abd1c3658e51b92fa001a04d0f336ce0e28
Suresh J <142098721+Suresh-15@users.noreply.github.com>
2024-09-01 19:42:46 +0530

    Time: 81 ms (61.08%) | Memory: 92.4 MB (45.59%) - LeetSync

--- 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.cpp
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
-        vector<vector<int>> result(m);
+        vector<vector<int>> result(m , vector<int>(n));
         if (m * n != original.size())
             return {};
 
         int index = 0;
         for (int i = 0; i < m; i++)
             for (int j = 0; j < n; j++)
-                result[i].push_back(original[index++]);
+                result[i][j] = original[index++];
 
         return result;
     }
