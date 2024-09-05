7ac83c151de333eb6f0bc2057d4855e3e8d09e22
Suresh J <142098721+Suresh-15@users.noreply.github.com>
2024-09-05 22:24:13 +0530

    Time: 18 ms (92.64%) | Memory: 11.7 MB (90.86%) - LeetSync

--- 2487-optimal-partition-of-string/optimal-partition-of-string.cpp
+++ 2487-optimal-partition-of-string/optimal-partition-of-string.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
     int partitionString(string s) {
-        int n = s.length();
-        int i = 0;
-        int count = 1;
-        unordered_map<char, int> mp;
-        while (i < n) {
-            if (mp.size() == 0) 
-                mp[s[i]] = 1;
-            else if (mp.find(s[i]) == mp.end()) 
-                mp[s[i]] = 1;
-            else {
-                count++;
-                mp.clear();
-                mp[s[i]] = 1;
+        std::array<bool, 26> att = {};
+        int count = 0;
+        for (auto ch : s) {
+            if (att[ch - 'a'] > 0) {
+                att.fill(false);
+                ++count;
             }
-            i++;
+            att[ch - 'a'] = true;
         }
+        ++count; 
         return count;
     }
 };
