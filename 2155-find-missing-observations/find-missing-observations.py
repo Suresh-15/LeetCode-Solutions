6108df3c71947416d4326de6089139c276dab320
Suresh J <142098721+Suresh-15@users.noreply.github.com>
2024-09-05 14:03:36 +0530

    Time: 980 ms (66.31%) | Memory: 26.7 MB (69.79%) - LeetSync

---
+++ 2155-find-missing-observations/find-missing-observations.py
@@ -0,0 +1,21 @@
+class Solution:
+    def missingRolls(self, rolls: List[int], mean: int, n: int) -> List[int]:
+        total_elements = len(rolls) + n
+        sum_of_all_elements = total_elements * mean
+        sum_of_second_list = sum_of_all_elements - sum(rolls)
+        if sum_of_second_list > 6 * n or sum_of_second_list < n:
+            return []
+
+        second_list = [1] * n
+        if sum(second_list) == sum_of_second_list:
+            return second_list
+        else:
+            missing_sum = sum_of_second_list - n
+            for i in range(n):
+                second_list[i] += min(5, missing_sum)
+                missing_sum -= 5
+                if missing_sum <= 0:
+                    break
+
+        return second_list
+            
