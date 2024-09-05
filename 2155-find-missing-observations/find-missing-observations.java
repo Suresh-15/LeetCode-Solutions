91ce5478e768e67956d57382aae50024a33c456e
Suresh J <142098721+Suresh-15@users.noreply.github.com>
2024-09-05 14:27:38 +0530

    Time: 3 ms (98.03%) | Memory: 59.8 MB (77.38%) - LeetSync

--- 2155-find-missing-observations/find-missing-observations.java
+++ 2155-find-missing-observations/find-missing-observations.java
@@ -1,32 +1,22 @@
 class Solution {
     public int[] missingRolls(int[] rolls, int mean, int n) {
-        int[] second_list = new int[] {};
-        int total_elements = rolls.length + n;
-        int sum_of_all_elements = mean * total_elements;
-        int sum_of_first_list = 0;
-        for (int i : rolls)
-            sum_of_first_list += i;
-        int sum_of_second_list = sum_of_all_elements - sum_of_first_list;
+        int[] result = new int[n];
+        int sum = 0;
+        for (int i : rolls) 
+            sum += i;
 
-        if (sum_of_second_list < n || sum_of_second_list > 6 * n)
-            return second_list;
+        int missingSum = mean * (n + rolls.length) - sum;
+        if (missingSum > 6 * n || missingSum < n) 
+            return new int[] {};
 
-        second_list = new int[n];
-        Arrays.fill(second_list, 1);
-        /*
-         * for (int i = 0; i < n; i++)
-         * second_list[i] = 1;
-         */
+        int expectedAvg = missingSum / n, remainder = missingSum % n;
 
-        sum_of_second_list -= n;
-        for (int i = 0; i < n; i++) {
-            second_list[i] += Math.min(5, sum_of_second_list);
-            sum_of_second_list -= 5;
-            if (sum_of_second_list <= 0)
-                break;
-        }
-
-        return second_list;
+        for (int i = 0; i < remainder; i++) 
+            result[i] = expectedAvg + 1;
+        for (int i = remainder; i < n; i++) 
+            result[i] = expectedAvg;
+        
+        return result;
 
     }
 }
