1594b19830fb836b3f985a0a6a9783495e8ea276
Suresh J <142098721+Suresh-15@users.noreply.github.com>
2024-09-05 14:32:41 +0530

    Time: 86 ms (87.50%) | Memory: 116.3 MB (69.12%) - LeetSync

--- 2155-find-missing-observations/find-missing-observations.cpp
+++ 2155-find-missing-observations/find-missing-observations.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     vector<int> missingRolls(vector<int>& rolls, int mean, int n) {
         int total = 0, m = rolls.size();
-        int m_sum = accumulate(rolls.begin(), rolls.end(),0);
-        long total_sum = (m+n)*mean;
-
+        int m_sum = accumulate(rolls.begin(), rolls.end(), 0);
+        long total_sum = (m + n) * mean;
 
         int n_sum = total_sum - m_sum;
         if (n_sum < n || n_sum > 6 * n)
-            return {}; 
+            return {};
 
         int distribution = n_sum / n, remaining = n_sum % n;
         vector<int> ans(n, distribution);
@@ -17,6 +16,5 @@ public:
             ++ans[i];
 
         return ans;
-
     }
 };
