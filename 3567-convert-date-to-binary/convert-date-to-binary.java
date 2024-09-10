08e6aa8c841da5eaf0c06733c8799eb785f5c14b
Suresh J <142098721+Suresh-15@users.noreply.github.com>
2024-09-10 22:23:10 +0530

    Time: 7 ms (64.04%) | Memory: 42.6 MB (58.88%) - LeetSync

--- 3567-convert-date-to-binary/convert-date-to-binary.java
+++ 3567-convert-date-to-binary/convert-date-to-binary.java
@@ -1,18 +1,13 @@
 class Solution {
-    public String bin(int num) {
-        StringBuilder s = new StringBuilder();
-        while (num > 0) {
-            s.append(String.valueOf(num % 2));
-            num = num / 2;
-        }
-        s.reverse();
-        return s.toString();
-    }
     public String convertDateToBinary(String date) {
         int y = Integer.parseInt(date.substring(0, 4));
         int m = Integer.parseInt(date.substring(5, 7));
         int d = Integer.parseInt(date.substring(8));
 
-        return bin(y) + "-" + bin(m) + "-" + bin(d);
+        String d1 = String.valueOf(Integer.toBinaryString(d));
+        String m1 = String.valueOf(Integer.toBinaryString(m));
+        String y1 = String.valueOf(Integer.toBinaryString(y));
+    
+        return y1 + "-" + m1 + "-" + d1;
     }
 }
