1b773adfec86612626cc61705fe783e308bf52ec
Suresh J <142098721+Suresh-15@users.noreply.github.com>
2024-09-12 10:09:13 +0530

    Time: 223 ms (42.85%) | Memory: 18.4 MB (83.05%) - LeetSync

--- 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.py
+++ 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.py
@@ -3,7 +3,7 @@ class Solution:
         count = 0
         characters = set(allowed)
         for word in words:
-            for char in word:
+            for char in set(word):
                 if char not in characters:
                     count += 1
                     break
