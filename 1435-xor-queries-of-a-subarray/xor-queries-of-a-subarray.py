4713a918587acec4a69d5272809b6683d9d680c7
Suresh J <142098721+Suresh-15@users.noreply.github.com>
2024-09-14 12:54:01 +0530

    Time: 303 ms (58.41%) | Memory: 31.3 MB (55.64%) - LeetSync

--- 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.py
+++ 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.py
@@ -5,8 +5,7 @@ class Solution:
         for i in range(len(arr)):
             prefix_xors.append(prefix_xors[i] ^ arr[i])
         for q in queries:
-            l, r = q[0], q[1]
-            output.append(prefix_xors[l] ^ prefix_xors[r + 1])
+            output.append(prefix_xors[q[0]] ^ prefix_xors[q[1] + 1])
         return output
         
             
