a3aeda20a282db4f04e2e371526c053766224ca3
Suresh J <142098721+Suresh-15@users.noreply.github.com>
2024-09-15 19:04:34 +0530

    Time: 4264 ms (100.00%) | Memory: 20.3 MB (100.00%) - LeetSync

--- 3575-find-the-maximum-sequence-value-of-array/find-the-maximum-sequence-value-of-array.py
+++ 3575-find-the-maximum-sequence-value-of-array/find-the-maximum-sequence-value-of-array.py
@@ -1,40 +1,27 @@
 class Solution:
     def maxValue(self, nums: List[int], k: int) -> int:
         n = len(nums)
-        max_num = max(nums)
-        max_or_value = (1 << (max_num.bit_length())) - 1
 
-        dp_forward = [{} for _ in range(n)]
-        dp_backward = [{} for _ in range(n)]
+        def get_possible(a):
+            t1 = {(0, 0)}
+            d = defaultdict(lambda: -1)
+            for i, x in enumerate(a):
+                t2 = set()
+                for taken, val in t1:
+                    if taken < k:
+                        t2.add((taken + 1, val | x))
+                        if taken + 1 == k and val | x not in d:
+                            d[val | x] = i + 1
+                t1.update(t2)
+            return d
 
-        # Initialize dp_forward
-        dp_forward[0][1] = {nums[0]}
-        for l in range(2, k + 1):
-            dp_forward[0][l] = set()
-        for i in range(1, n):
-            dp_forward[i][1] = dp_forward[i - 1][1] | {nums[i]}
-            for l in range(2, k + 1):
-                dp_forward[i][l] = dp_forward[i - 1][l] | {
-                    or_val | nums[i] for or_val in dp_forward[i - 1][l - 1]
-                }
+        a = get_possible(nums)
+        b = get_possible(nums[::-1])
+        ans = -float("inf")
 
-        # Initialize dp_backward
-        dp_backward[n - 1][1] = {nums[n - 1]}
-        for l in range(2, k + 1):
-            dp_backward[n - 1][l] = set()
-        for i in range(n - 2, -1, -1):
-            dp_backward[i][1] = dp_backward[i + 1][1] | {nums[i]}
-            for l in range(2, k + 1):
-                dp_backward[i][l] = dp_backward[i + 1][l] | {
-                    or_val | nums[i] for or_val in dp_backward[i + 1][l - 1]
-                }
+        for v1, x in a.items():
+            for v2, y in b.items():
+                if x + y <= n:
+                    ans = max(ans, v1 ^ v2)
 
-        max_value = 0
-        for i in range(k - 1, n - k):
-            forward_ors = dp_forward[i][k] if k in dp_forward[i] else set()
-            backward_ors = dp_backward[i + 1][k] if k in dp_backward[i + 1] else set()
-            for or1 in forward_ors:
-                for or2 in backward_ors:
-                    max_value = max(max_value, or1 ^ or2)
-
-        return max_value
+        return ans
