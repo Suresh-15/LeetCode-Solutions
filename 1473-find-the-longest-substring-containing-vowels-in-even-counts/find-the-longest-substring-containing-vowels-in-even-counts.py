820bcfd049674e19c96805403113f84bd4ea62e0
Suresh J <142098721+Suresh-15@users.noreply.github.com>
2024-09-15 11:18:51 +0530

    Time: 155 ms (91.64%) | Memory: 22.1 MB (33.15%) - LeetSync

--- 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.py
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/find-the-longest-substring-containing-vowels-in-even-counts.py
@@ -1,23 +1,18 @@
 class Solution:
     def findTheLongestSubstring(self, s: str) -> int:
-        def function(string):
-            h = {'a': 0, 'e': 0, 'i':0, 'o': 0, 'u':0}
-            for i in string:
-                if i in h.keys():
-                    h[i] += 1
-            
-            print(h)
-            for j in h.values():
-                if j % 2 != 0:
-                    return False
-            else:
-                return True
+        num_bitmasks = 0b100000
+        char_masks = [0] * (ord("z") + 1)
+        for i, c in enumerate("aeiou"):
+            char_masks[ord(c)] = 1 << i
 
-        k = n = len(s)
-        while k > 0:
-            for i in range(n - k + 1):
-                if function(s[i : i + k]):
-                    return k
-            k -= 1
+        curr_prefix = 0
+        first_index_of = [-1] * num_bitmasks
+        last_index_of = [-1] * num_bitmasks
+        for i, c in enumerate(s):
+            curr_prefix ^= char_masks[ord(c)]
+            if first_index_of[curr_prefix] == -1:
+                first_index_of[curr_prefix] = i
+            last_index_of[curr_prefix] = i
+        first_index_of[0] = -1
 
-        return 0
+        return max(b - a for a, b in zip(first_index_of, last_index_of))
