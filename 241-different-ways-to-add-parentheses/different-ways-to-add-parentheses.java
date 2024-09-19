1308ce12c75eb7b0d423208580ddc0a2d8f56d47
Suresh J <142098721+Suresh-15@users.noreply.github.com>
2024-09-19 19:21:44 +0530

    Time: 1 ms (99.68%) | Memory: 41.4 MB (98.16%) - LeetSync

--- 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.java
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.java
@@ -1,24 +1,54 @@
 class Solution {
-    public List<Integer> diffWaysToCompute(String expression) {
-        List<Integer> result = new ArrayList<>();
-        for (int i = 0; i < expression.length(); ++i) {
-            char oper = expression.charAt(i);
-            if (oper == '+' || oper == '-' || oper == '*') {
-                List<Integer> s1 = diffWaysToCompute(expression.substring(0, i));
-                List<Integer> s2 = diffWaysToCompute(expression.substring(i + 1));
-                for (int a : s1) {
-                    for (int b : s2) {
-                        if (oper == '+')
-                            result.add(a + b);
-                        else if (oper == '-') 
-                            result.add(a - b);
-                        else if (oper == '*') 
-                            result.add(a * b);
-                    }
+    List<Integer>[][] dp;
+
+    public List<Integer> diffWaysToCompute(String ep) {
+        dp = new ArrayList[ep.length()][ep.length()];
+        return fun(ep, 0, ep.length() - 1);
+    }
+
+    private List<Integer> fun(String a, int start, int end) {
+        List<Integer> ret = new ArrayList<>();
+        if (dp[start][end] != null)
+            return dp[start][end];
+        int x = operand(a, start, end);
+        if (x != -1) {
+            ret.add(x);
+            dp[start][end] = ret;
+            return ret;
+        }
+        for (int i = start; i <= end; i++) {
+            if (!isOp(a.charAt(i)))
+                continue;
+            List<Integer> left = fun(a, start, i - 1);
+            List<Integer> right = fun(a, i + 1, end);
+            for (int j = 0; j < left.size(); j++) {
+                for (int k = 0; k < right.size(); k++) {
+                    if (a.charAt(i) == '*')
+                        ret.add(left.get(j) * right.get(k));
+                    if (a.charAt(i) == '+')
+                        ret.add(left.get(j) + right.get(k));
+                    if (a.charAt(i) == '-')
+                        ret.add(left.get(j) - right.get(k));
                 }
             }
         }
-        if (result.isEmpty()) result.add(Integer.parseInt(expression));
-        return result;
+        dp[start][end] = ret;
+        return ret;
     }
+
+    private int operand(String a, int start, int end) {
+        for (int i = start; i <= end; i++) {
+            if (isOp(a.charAt(i)))
+                return -1;
+        }
+        int value = 0;
+        for (int i = start; i <= end; i++)
+            value = value * 10 + (a.charAt(i) - '0');
+        return value;
+    }
+
+    private boolean isOp(char c) {
+        return c == '*' || c == '-' || c == '+';
+    }
+
 }
