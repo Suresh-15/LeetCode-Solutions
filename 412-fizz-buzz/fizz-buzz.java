1efe572fdb8ba4343927378776e235d593f2ced6
Suresh J <142098721+Suresh-15@users.noreply.github.com>
2024-09-22 00:28:32 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (90.56%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.java
+++ 412-fizz-buzz/fizz-buzz.java
@@ -1,17 +1,21 @@
 class Solution {
     public List<String> fizzBuzz(int n) {
-        String str;
-        List<String> result = new ArrayList<>();
-        for (int i = 1; i <= n; i++) {
-            str = "";
-            if (i % 3 == 0)
-                str += "Fizz";
-            if (i % 5 == 0)
-                str += "Buzz";
-            if (str == "")
-                str = String.valueOf(i);
-            result.add(str);
-        }
-        return result;
+        return new java.util.AbstractList<String>() {
+            @Override
+            public String get(int i) {
+                i++;
+                return (
+                    i % 15 == 0 ? "FizzBuzz" :
+                    i % 5 == 0 ? "Buzz" :
+                    i % 3 == 0 ? "Fizz" :
+                    String.valueOf(i)
+                );
+            }
+            @Override
+            public int size() {
+                return n;
+            }
+        };
+
     }
 }
