5b9260cec0c133d5fe5c2d39d30c5d09dd90f448
Suresh J <142098721+Suresh-15@users.noreply.github.com>
2024-09-22 00:24:43 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (87.85%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.cpp
+++ 412-fizz-buzz/fizz-buzz.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     vector<string> fizzBuzz(int n) {
+        string str;
         vector<string> ans(n);
         for (int i = 1; i <= n; i++) {
-            string str;
+            str = "";
             if (i % 3 == 0)
                 str += "Fizz";
             if (i % 5 == 0)
                 str += "Buzz";
-            if (str.empty())
+            if (str == "")
                 str = to_string(i);
             ans[i - 1] = str;
         }
