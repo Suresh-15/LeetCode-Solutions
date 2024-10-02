57962a9d94b58c8b63887ad199699731822ac2a0
Suresh J <142098721+Suresh-15@users.noreply.github.com>
2024-10-02 12:14:16 +0530

    Time: 29 ms (50.91%) | Memory: 61.8 MB (66.94%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.java
@@ -1,26 +1,23 @@
 class Solution {
-    
-    public int[] sort_and_unique(int[] arr) {
-        Set<Integer> set = new HashSet<>();
-        for(int num : arr) {
-            set.add(num);
-        }    
-        int[] nums = set.stream().mapToInt(Integer :: intValue).toArray();
-        Arrays.sort(nums);
-        return nums;
-    }
-
     public int[] arrayRankTransform(int[] arr) {
-        int[] nums = sort_and_unique(arr);
-        Map<Integer, Integer> map = new HashMap<>();
+        if (arr.length == 0) {
+            return arr;
+        }
+        int[] nums = arr.clone();
+        Arrays.sort(nums);
+        HashMap<Integer, Integer> map = new HashMap<>();
         int rank = 1;
-        for (int num : nums) {
-            map.put(num, rank++);
+        map.put(nums[0], rank++);
+        for (int i = 1; i < arr.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                continue;
+            } else {
+                map.put(nums[i], rank++);
+            }
         }
-        int[] ranks = new int[arr.length];
         for (int i = 0; i < arr.length; i++) {
-            ranks[i] = map.get(arr[i]);
+            nums[i] = map.get(arr[i]);
         }
-        return ranks;
+        return nums;
     }
 }
