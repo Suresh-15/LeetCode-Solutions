9e9f40570ee181e7cd776d92fab54e08ad3f2c48
Suresh J <142098721+Suresh-15@users.noreply.github.com>
2024-10-02 11:13:28 +0530

    Time: 126 ms (98.41%) | Memory: 112.9 MB (99.44%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -11,33 +11,36 @@
 
 class Solution {
 public:
-    ListNode* reverseList(ListNode* head) {
-        ListNode *reverse = NULL;
-        ListNode *current = head;
-        while(current != NULL) {
-            ListNode *next = current->next;
-            current->next = reverse;
-            reverse = current;
-            current = next;
-        }
-        return reverse;
-    }
     bool isPalindrome(ListNode* head) {
-        ListNode* slow = head;
-        ListNode* fast = head->next;
-        while (fast != NULL && fast->next != NULL) {
-            slow = slow->next;
-            fast = fast->next->next;
+        ListNode* temp1 = head;
+        int length = 0;
+        while (temp1 != nullptr) {
+            length++;
+            temp1 = temp1->next;
         }
 
-        ListNode* reverse = reverseList(slow->next);
-        slow->next = NULL;
-        while (reverse != NULL) {
-            if (head->val != reverse->val) {
+        ListNode *temp, *prev = nullptr;
+        ListNode* temp2 = head;
+        temp1 = head;
+        while (temp2 != nullptr && temp2->next != nullptr) {
+            temp1 = temp1->next;
+            temp2 = temp2->next->next;
+        }
+        while (head != temp1) {
+            temp = head->next;
+            head->next = prev;
+            prev = head;
+            head = temp;
+        }
+        if (length % 2 == 1)
+            temp1 = temp1->next;
+        temp2 = temp1;
+        while (prev != temp1 && temp2 != nullptr) {
+            if (prev->val == temp1->val) {
+                prev = prev->next;
+                temp1 = temp1->next;
+            } else
                 return false;
-            }
-            head = head->next;
-            reverse = reverse->next;
         }
         return true;
     }
