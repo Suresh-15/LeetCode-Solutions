49b587ab52dff2a799a25e2d01a4e7db98de90de
Suresh J <142098721+Suresh-15@users.noreply.github.com>
2024-10-10 18:12:01 +0530

    Time: 47 ms (53.17%) | Memory: 16.6 MB (56.80%) - LeetSync

--- 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.py
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.py
@@ -1,3 +1,18 @@
 class Solution:
     def kthCharacter(self, k: int) -> str:
-        return chr(ord('a') + (k - 1).bit_count())
+        nextChar = {
+            'a':'b', 'b':'c', 'c':'d', 'd':'e', 'e':'f', 'f':'g', 
+            'g':'h', 'h':'i', 'i':'j', 'j':'k', 'k':'l', 'l':'m', 
+            'm':'n', 'n':'o', 'o':'p', 'p':'q', 'q':'r', 'r':'s', 
+            's':'t', 't':'u', 'u':'v', 'v':'w', 'w':'x', 'x':'y',
+            'y':'z', 'z':'a'
+        }
+
+        string = 'ab'
+        appendString = 'b'
+        while len(string) < k:
+            for i in range(len(appendString)):
+                appendString += nextChar[appendString[i]]
+            string += appendString
+    
+        return string[k - 1]
