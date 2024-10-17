e3457cf45eb4154f287b10c7aeacf1a9b315258f
Suresh J <142098721+Suresh-15@users.noreply.github.com>
2024-10-18 01:12:03 +0530

    Time: 0 ms (100.00%) | Memory: 16.6 MB (10.93%) - LeetSync

---
+++ 670-maximum-swap/maximum-swap.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maximumSwap(self, num: int) -> int:
+        num_str = list(str(num))
+        n = len(num_str)
+        max_right_index = [0] * n
+
+        max_right_index[n - 1] = n - 1
+        for i in range(n - 2, -1, -1):
+            max_right_index[i] = (
+                i
+                if num_str[i] > num_str[max_right_index[i + 1]]
+                else max_right_index[i + 1]
+            )
+
+        for i in range(n):
+            if num_str[i] < num_str[max_right_index[i]]:
+                num_str[i], num_str[max_right_index[i]] = (
+                    num_str[max_right_index[i]],
+                    num_str[i],
+                )
+                return int("".join(num_str))
+
+        return num
